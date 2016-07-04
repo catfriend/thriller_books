0142d714b39603d9792f3257348d5e7afbe48bcc
catfriend <eakari@live.com>
2016-07-04 15:56:07 -0700

    set up gulp and nodemon

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-var express = require ('express');
+var express = require('express');
 
-var app = express(); //gives instance of express
+var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-
-app.get('/', function(req, res) {
-  res.send('hello world')
+app.get('/', function (req, res) {
+    res.send('Hello World');
 });
 
-app.listen(5000, function(err) {
-  console.log('running server on port 5000')
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
