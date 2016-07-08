67b57eba99f032363ab65b8d52d8bf3e643c2494
catfriend <eakari@live.com>
2016-07-08 10:39:47 -0700

    fixed typo

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.set('views', '.src/views');
+app.set('views', './src/views');
+
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
