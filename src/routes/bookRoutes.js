ac8b2cfbd306fd33619818499a91d8bfa5d5e9f4
catfriend <eakari@live.com>
2016-07-09 16:38:16 -0700

    passport authentication

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,6 +5,12 @@ var objectId = require('mongodb').ObjectID;
 
 var router = function(nav){
 
+    bookRouter.use(function (req, res, next) {
+            if (!req.user) {
+                res.redirect('/');
+            }
+            next();
+    });
     bookRouter.route('/')
     .get(function (req, res) {
          var url = 
